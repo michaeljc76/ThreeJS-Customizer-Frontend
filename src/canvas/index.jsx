9869c5cdc3a44fdcb325b92e44e98a46c3e576f7
Michael Crowley <nico404.site@gmail.com>
2023-11-05 01:00:50 -0400

    Update index.jsx

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,7 +1,7 @@
 import { Canvas } from '@react-three/fiber'
 import { Environment, Center } from '@react-three/drei'
 import Shirt from './Shirt'
-import BackDrop from './backdrop'
+import BackDrop from './BackDrop'
 import CameraRig from './CameraRig'
 
 const CanvasModel = () => {
